b2c06b05c785731d854b8026c06a8a32f52282f7
karad <kazuhiroh+karad@gmail.com>
2015-05-08 19:36:48 +0900

    update to electron 0.25.3

--- resources/leiningen/new/descjop/Gruntfile.js
+++ resources/leiningen/new/descjop/Gruntfile.js
@@ -1,14 +1,14 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    "download-electron": {
-      version: "0.24.0",
-      outputDir: "./electron",
-      rebuild: true
-    }
-  });
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		"download-electron": {
+			version: "0.25.3",
+			outputDir: "./electron",
+			rebuild: true
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-download-electron');
+	grunt.loadNpmTasks('grunt-download-electron');
 
 };
